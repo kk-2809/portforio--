0239c4f39145365debd2775181f617bc6c15c686
kk-2809 <koko.rila.928@gmail.com>
2025-05-29 14:23:52 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,19 @@
     nav.classList.toggle('active');
   });
 }
+
+{
+  const ham = document.querySelector('#js-hamburger');
+  const Ul = document.getElementById('js-nav');
+  const currentActive = document.querySelectorAll('#menu');
+  currentActive.forEach(entry => {
+    entry.addEventListener('click', () => {
+      if (Ul.classList.contains('active') == true) {
+        Ul.classList.remove('active');
+        ham.classList.remove('active');
+      } return;
+    });
+  });
+}
+
+
